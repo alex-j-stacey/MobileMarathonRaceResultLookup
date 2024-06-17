82f4a7f47ed19d65d6bfba4ac1caaa2c5623c02e
Alex Stacey <astacey@hbs.net>
2024-06-17 11:09:20 -0500

    uploading mobile app to github

---
+++ Marathon/Models/Race.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Marathon.Models
+{
+    public class RaceCollection
+    {
+        public Race[] races { get; set; }
+    }
+
+    public class Race
+    {
+        public int id { get; set; }
+        public string race_name { get; set; }
+    }
+}
+
